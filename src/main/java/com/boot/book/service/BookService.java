838b5632037c50750eb5e60879bbc34a977742d6
manamjong <nskynet5374@naver.com>
2025-05-13 14:51:28 +0900

    책(기본,추천,리뷰 분리)(**기록 분리해야함**)

--- src/main/java/com/boot/book/service/BookService.java
+++ src/main/java/com/boot/book/service/BookService.java
@@ -47,43 +47,4 @@ public interface BookService {
 	public int getRecordTotalCount(@Param("criteria") UserBookBorrowingCriteriaDTO criteria,
 			@Param("userNumber") int userNumber);
 
-	public int checkReview(HashMap<String, String> param);
-
-	public ArrayList<ReviewDTO> getReview(@Param("criteria") NoticeCriteriaDTO criteria,
-			@Param("param") HashMap<String, String> param);
-
-	public int getReviewCount(@Param("criteria") NoticeCriteriaDTO criteria,
-			@Param("param") HashMap<String, String> param);
-
-	public int insertReview(HashMap<String, String> param);
-
-	public int updateReview(ReviewDTO reviewDTO);
-
-	public int deleteReview(ReviewDTO reviewDTO);
-
-	public ReviewDTO getReviewById(int reviewId);
-
-	// 도서별 리뷰 통계 조회 (페이징 없이 모든 리뷰 가져오기)
-	public ArrayList<ReviewDTO> getAllReviewsByBookNumber(int bookNumber);
-
-	// 리뷰 도움됨 추가
-	boolean addReviewHelpful(int reviewId, int userNumber);
-
-	// 리뷰 도움됨 취소
-	boolean removeReviewHelpful(int reviewId, int userNumber);
-
-	// 리뷰 도움됨 여부 확인
-	boolean checkReviewHelpful(int reviewId, int userNumber);
-
-	// 리뷰별 도움됨 개수 조회
-	int getReviewHelpfulCount(int reviewId);
-
-	public ArrayList<BookDTO> Top5Recommend(HashMap<String, String> param);
-
-	public ArrayList<BookDTO> Top3Borrow();
-
-	public ArrayList<BookDTO> Top5Random(HashMap<String, String> param);
-
-	public String CategoryNum(HashMap<String, String> param);
-
 }
